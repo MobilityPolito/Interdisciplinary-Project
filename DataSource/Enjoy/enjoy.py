fb308d9fb0947f18b9aa450b8c39d78c63b0a6be
Andrian putina <anr.putina@gmail.com>
2016-11-15 23:28:38 +0100

    Added main loop and first check session into enjoy script

--- DataSource/Enjoy/enjoy.py
+++ DataSource/Enjoy/enjoy.py
@@ -1,19 +1,33 @@
 import requests
 import json
-#import cookielib
+import time
 
 URL_HOME = 'https://enjoy.eni.com'
 URL_COOKIE = 'https://enjoy.eni.com/ajax/set_cookie_pref_city'
 URL_CARS =  'https://enjoy.eni.com/ajax/retrieve_vehicles'
 
-session = requests.Session()
 data_city = {'city': 'torino'}
 
-session.get(URL_HOME)
-session.post(URL_COOKIE, data=json.dumps(data_city))
-r=session.get(URL_CARS)
+def main():
 
+	checkSession = False;
+	while (1):
 
-#print(r.text)
-with open('prova.txt','w+') as outputfile:
-	outputfile.write(r.text) 
+		#check if session is still valid (not implemented yet)
+		if not checkSession: 
+			session = requests.Session()
+			session.get(URL_HOME)
+			session.post(URL_COOKIE, data=json.dumps(data_city))
+			checkSession = True
+		
+		request = session.get(URL_CARS)
+
+		#Write response on file for now
+		#Later the response will be pushed into Mongodb
+		with open('prova.txt','a') as outputfile:
+			outputfile.write(request.text)
+
+		time.sleep(60) 
+
+if __name__ == '__main__':
+	main()
