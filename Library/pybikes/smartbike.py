74c7b883cb5a221641ad208604aac1773f654080
Andrian Putina <anr.putina@gmail.com>
2016-12-02 15:46:56 +0100

    added library

---
+++ Library/pybikes/smartbike.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
+# Distributed under the AGPL license, see LICENSE.txt
+
+import re
+import json
+from lxml import html
+
+from .base import BikeShareSystem, BikeShareStation
+from . import utils
+
+LAT_LNG_RGX = 'point \= new GLatLng\((.*?)\,(.*?)\)'
+ID_ADD_RGX = 'idStation\=(.*)\&addressnew\=(.*)\&s\_id\_idioma'
+ID_ADD_RGX_V = 'idStation\=\"\+(.*)\+\"\&addressnew\=(.*)\+\"\&s\_id\_idioma'
+
+parse_methods = {
+    'xml': 'get_xml_stations',
+    'json': 'get_json_stations',
+    'json_v2': 'get_json_v2_stations'
+}
+
+
+class BaseSystem(BikeShareSystem):
+    meta = {
+        'system': 'SmartBike',
+        'company': ['ClearChannel']
+    }
+
+
+class SmartBike(BaseSystem):
+    sync = True
+
+    def __init__(self, tag, meta, feed_url, format="json"):
+        super(SmartBike, self).__init__(tag, meta)
+        self.feed_url = feed_url
+        if format not in parse_methods:
+            raise Exception('Unsupported method %s' % format)
+        self.method = parse_methods[format]
+
+    def update(self, scraper=None):
+        if scraper is None:
+            scraper = utils.PyBikesScraper()
+
+        raw_req = scraper.request(self.feed_url)
+        self.stations = eval(self.method)(self, raw_req)
+
+
+def get_xml_stations(self, raw):
+    raise Exception("Not implemented")
+
+
+def get_json_stations(self, raw):
+    # Double encoded json FTWTF..
+    data = json.loads(json.loads(raw)[1]['data'])
+    stations = map(SmartBikeStation, data)
+    return stations
+
+
+def get_json_v2_stations(self, raw):
+    data = json.loads(raw)
+    stations = map(SmartBikeStation, data)
+    return stations
+
+
+class SmartBikeStation(BikeShareStation):
+    def __init__(self, info):
+        super(SmartBikeStation, self).__init__()
+        try:
+            self.name = info['StationName']
+            self.bikes = int(info['StationAvailableBikes'])
+            self.free = int(info['StationFreeSlot'])
+            self.latitude = float(info['AddressGmapsLatitude'])
+            self.longitude = float(info['AddressGmapsLongitude'])
+            self.extra = {
+                'uid': int(info['StationID']),
+                'status': info['StationStatusCode'],
+                'districtCode': info['DisctrictCode'],
+                'NearbyStationList': map(
+                    int, info['NearbyStationList'].split(',')
+                )
+            }
+        except KeyError:
+            # Either something has changed, or it's the other type of feed
+            # Same data, different keys.
+            self.name = info['name']
+            self.bikes = int(info['bikes'])
+            self.free = int(info['slots'])
+            self.latitude = float(info['lat'])
+            self.longitude = float(info['lon'])
+            self.extra = {
+                'uid': int(info['id']),
+                'status': info['status']
+            }
+            if 'address' in info:
+                self.extra['address'] = info['address']
+            if 'district' in info:
+                self.extra['districtCode'] = info['district']
+            elif 'districtCode' in info:
+                self.extra['districtCode'] = info['districtCode']
+
+            nearby_stations = info.get('nearbyStations')
+            if nearby_stations and nearby_stations != "0":
+                self.extra['NearbyStationList'] = map(
+                    int, nearby_stations.split(',')
+                )
+
+            if 'zip' in info and info['zip']:
+                self.extra['zip'] = info['zip']
+
+            if 'stationType' in info and \
+                    info['stationType'] == 'ELECTRIC_BIKE':
+                self.extra['ebikes'] = True
+
+
+class SmartShitty(BaseSystem):
+    """
+    BikeMI decided (again) to implement yet another way of displaying the map...
+    This time the data come in a less messy way in comparison to the previous version,
+    but the HTML comes unescaped (escaped bellow for better understanding).
+    Who the fuck pay this guys money, seriously?
+
+    GoogleMap.addMarker(
+        '/media/assets/images/station_map/more_than_five_bikes_flag.png',
+        45.464683238626,
+        9.18879747390747,
+        'Duomo',
+        '<div style="width: 240px; height: 120px;">
+            <span style="font-weight: bold;">178 - V Alpini</span>
+            <br>
+            <ul>
+                <li>Available bicycles: 9</li>
+                <li>Available electrical bicycles: 0</li>
+                <li>Available slots: 20</li>
+                </ul>
+        </div>');
+    """
+    sync = True
+
+    RGX_MARKERS = r'GoogleMap\.addMarker\(.*?,\s*(\d+.\d+)\s*,\s*(\d+.\d+),\s*\'(.*?)\',(.*?)\)\;'
+
+    def __init__(self, tag, meta, feed_url):
+        super(SmartShitty, self).__init__(tag, meta)
+        self.feed_url = feed_url
+
+    def update(self, scraper=None):
+        if scraper is None:
+            scraper = utils.PyBikesScraper()
+
+        page = scraper.request(self.feed_url)
+        stations_data = re.findall(SmartShitty.RGX_MARKERS,
+                                   page.encode('utf-8'))
+        stations = []
+        stats_query = """
+            //td[span[text() = "%s"]]/
+                following-sibling::td/text()
+        """
+
+        stats_rules = {
+            'std': 'Bicycles',
+            'ebikes': 'Electric bicycles',
+            'kids_bikes': 'Bicycles for kids'
+        }
+
+        for station_data in stations_data:
+            latitude, longitude, name, mess = station_data
+            html_mess = html.fromstring(mess.decode('unicode_escape'))
+            stats = {}
+            bikes = 0
+            extra = {}
+
+            for k, rule in stats_rules.iteritems():
+                stats[k] = map(int, html_mess.xpath(stats_query % rule))
+
+            if stats['std']:
+                bikes += stats['std'][0]
+                free = stats['std'][1]
+
+            if stats['ebikes'] and stats['ebikes'][0] > 0:
+                bikes += stats['ebikes'][0]
+                extra['ebikes'] = stats['ebikes'][0]
+                extra['has_ebikes'] = True
+            
+            if stats['kids_bikes'] and stats['kids_bikes'][0] > 0:
+                bikes += stats['kids_bikes'][0]
+                extra['kids_bikes'] = stats['kids_bikes'][0]
+                extra['has_kids_bikes'] = True
+
+            station = BikeShareStation(name, float(latitude), float(longitude),
+                                       bikes, free, extra)
+            stations.append(station)
+        self.stations = stations
---
+++ Library/pybikes/veloway.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010-2016, eskerda <eskerda@gmail.com>
+# Distributed under the AGPL license, see LICENSE.txt
+
+import re
+import json
+import urllib
+from lxml import html
+
+from .base import BikeShareSystem, BikeShareStation
+from . import utils
+
+
+class BaseSystem(BikeShareSystem):
+    meta = {
+        'system': 'Veloway',
+        'company': ['Veolia']
+    }
+
+
+class Veloway(BaseSystem):
+    def __init__(self, tag, meta, feed_url):
+        super(Veloway, self).__init__(tag, meta)
+        self.feed_url = feed_url
+
+    def update(self, scraper=None):
+        if scraper is None:
+            scraper = utils.PyBikesScraper()
+
+        data = json.loads(scraper.request(self.feed_url))
+        stations = []
+        for info in data['stand']:
+            try:
+                station = VelowayStation(info)
+            except Exception:
+                continue
+            stations.append(station)
+        self.stations = stations
+
+
+class VelowayStation(BikeShareStation):
+    """ A "stand" is like:
+        {
+          "wcom": "Boulevard+Rene+Cassin+(face+au+n%c2%b0100)+",
+          "disp": "1",      # available
+          "neutral": "0",   # neutralized or under construction, unused here
+          "lng": "7.2170729637146",
+          "lat": "43.6695556640625",
+          "tc": "15",       # total capacity
+          "ac": "15",       # available capacity
+          "ap": "7",        # available parking
+          "ab": "8",        # available bike
+          "id": "11",
+          "name": "Station+n%c2%b0011"
+        }
+    """
+    def __init__(self, info):
+        super(VelowayStation, self).__init__()
+        self.name = urllib.unquote_plus(
+            info['name'].decode('latin-1').encode('utf-8')
+        )
+        self.bikes = int(info['ab'])
+        self.free = int(info['ap'])
+        self.latitude = float(info['lat'])
+        self.longitude = float(info['lng'])
+        self.extra = {
+            'uid': int(info['id']),
+            'slots': int(info['tc']),
+            'slots_available': int(info['ac']),
+        }
+
+        if int(info['disp']) == 1:
+            self.extra['status'] = 'OPEN'
+        else:
+            self.extra['status'] = 'CLOSED'
+
+        if info['wcom']:
+            self.extra['address'] = urllib.unquote_plus(
+                info['wcom'].decode('latin-1').encode('utf-8')
+            )
+
+        if self.latitude is None or self.longitude is None:
+            raise Exception('A station needs a lat/lng to be defined!')
+        if self.latitude == 0 and self.longitude == 0:
+            raise Exception('A station can\'t be located in Atlantic Ocean!')
+
+
+class VelowayDrupal(Veloway):
+
+    # Station 01 - Gare SNCF (Vélos libres : 9 Places libres
+    station_rgx = (ur'Station\s+(?P<id>\d+)\s*-\s*'
+                   ur'(?P<name>.+)\s+'
+                   ur'\(Vélos\s+libres\s*:\s*(?P<bikes>\d+)\s+'
+                   ur'Places\s+libres\s*:\s*(?P<free>\d+)\)')
+
+    def update(self, scraper=None):
+        scraper = scraper or utils.PyBikesScraper()
+
+        _html = scraper.request(self.feed_url)
+        dom = html.fromstring(_html)
+        # Data is duplicated...
+        markers = dom.xpath("""
+            (//ul[li[@data-gmapping]])[1]//
+                li[@data-gmapping]
+        """)
+        stations = []
+        for marker in markers:
+            g_map = json.loads(marker.get('data-gmapping'))
+            lat = float(g_map['latlng']['lat'])
+            lng = float(g_map['latlng']['lng'])
+
+            fuzzle = ''.join(marker.xpath('.//text()')).strip()
+            data = re.search(self.station_rgx, fuzzle)
+
+            uid = data.group('id')
+            name = data.group('name')
+            bikes = int(data.group('bikes'))
+            free = int(data.group('free'))
+
+            extra = {
+                'uid': uid
+            }
+            station = BikeShareStation(name, lat, lng, bikes, free, extra)
+            stations.append(station)
+        self.stations = stations
