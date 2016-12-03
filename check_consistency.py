bf68a799b7c38860d18b99e854891621d6e77dee
Andrian putina <anr.putina@gmail.com>
2016-12-03 23:31:07 +0100

    added check over turin, db in remote!

--- check_consistency.py
+++ check_consistency.py
@@ -2,23 +2,34 @@ from pymongo import MongoClient
 import datetime
 import json
 
-MONGO_HOME = 'mongodb://localhost:27017/'
+MONGO_HOME = 'mongodb://64.137.240.27:27017/'
 
 client = MongoClient(MONGO_HOME)
-input_db = client['MobilityDataLake_2']
+input_db = client.CSMS
+input_db.authenticate('csms', '1234')
 collection = input_db['torino']
 
-start = datetime.datetime(2016, 12, 1)
-end = datetime.datetime.now()
+#start = datetime.datetime(2016, 12, 1)
+#end = datetime.datetime.now()
 
-cursor = collection.find({"timestamp":{'$gte': start, '$lt': end}})
+#cursor = collection.find({"timestamp":{'$gte': start, '$lt': end}})
+cursor = collection.find()
 
 count = 0
+count_error = 0
+
 for document in cursor:
 
-    if document["provider"] == "enjoy":
-        car = document["state"][0]
-        if car["lon"] < 8:
-            count += 1
+	try:
+		if document['provider'] == 'enjoy':
+			car = document['state'][0]
+			if car['lon'] < 8:
+				count += 1
+				print count
+	except:
+		print document['_id']
+		count_error += 1
 
-print count
+print "---------"
+print "Tot enjoy torino: ", count
+print "Tor error: ", count_error
