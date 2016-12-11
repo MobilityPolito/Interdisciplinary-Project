9c705196bd232d05e6b66f3e05d4fae745154991
michelelt <michelecocca.mc@gmail.com>
2016-12-11 12:47:38 +0100

    Working tobike script + of two days scraping

--- DataSource/tobike/tobike.py
+++ DataSource/tobike/tobike.py
@@ -10,28 +10,40 @@ import json
 
 MONGO_HOME = 'mongodb://localhost:27017/'
 DB_NAME = 'CSMS'
+LOG_PATH = "/home/mc/Scrivania/tobike/"
+tobike_url = "http://www.tobike.it/frmLeStazioni.aspx"
 
 client = MongoClient(MONGO_HOME)
 db = client.CSMS
-db.authenticate('csms', '1234')
+#db.authenticate('csms', '1234')
 
 client = MongoClient(MONGO_HOME)
 db = client[DB_NAME]
 
-def DBinsert(provider,city, current_state):
+
+def write_log (message):
+    with open(LOG_PATH+"tobike.log", "a+") as f:
+        timestamp = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+        f.write("[" + timestamp + "] -> " + message + "\n")
+
+        
+def DB_insert(provider,city, current_state):
     collection = db[city]
-    record = {\
-         "timestamp": datetime.datetime.now(),\
-         "provider": provider,\
-         "city" : city, \
-         "state": current_state\
+    record = {
+         "timestamp": str(datetime.datetime.now()),
+         "provider": provider,
+         "city" : city,
+         "state": current_state
          }
+#    with open("tobike.log", "a+") as f:
+#         f.write(json.dumps(record) + "\n")
      
-#    return record
+##    return record
     try:
         collection.insert_one(record)
+        write_log(TB.provider + " " + TB.city + ": record inserted")
     except:
-        print "Invalid data coding!"
+        write_log(TB.provider + " " + TB.city + ": Invalid data coding!")
         
 class TObike():
     
@@ -39,6 +51,8 @@ class TObike():
         self.provider = provider
         self.city = city
         self.last_state = None
+        self.session= None
+        self.obj = None
     
     #class counting the bikes' status for station 
     def tobike_bikes(self,bikes):
@@ -56,14 +70,19 @@ class TObike():
             'available_bikes': available_bikes,
             'broken_bikes': broken_bikes,
         }
-    
-    def get_data(self):
-        r = requests.get("http://www.tobike.it/frmLeStazioni.aspx")
-        r.raise_for_status()
         
+    def get_session(self,url):
+        try:
+            self.session = requests.get(url)
+            self.session.raise_for_status()
+            write_log(self.provider + " " + self.city + ": session successfully started")
+        except requests.exceptions.RequestException as err:  # This is the correct syntax
+            write_log(TB.provider + " " + TB.city + ": HTTP error:("+str(err)+")")
+                
+    def get_data(self):
         #REGEX expression to scrape the data
         RE = r"{RefreshMap\((?P<data>.*)\)}"
-        result = re.search(RE, r.text, re.UNICODE).group("data")
+        result = re.search(RE, self.session.text, re.UNICODE).group("data")
         
         row = result.split("','")
     
@@ -93,31 +112,41 @@ class TObike():
         d={}
         #print "++++"+ids[0]
         for i in range (num_points) :
-            #print ids[i]
-            d={
-            'id': str(ids[i]),
-            'onMapID': i,
-            'num_votes': num_votes[i],
-            'vote': votes[i],
-            'lat': lats[i],
-            'lng': lngs[i],
-            'name': names[i],
-            'bikes': self.tobike_bikes(bikes[i]),
-            'address': addresses[i],
-            'status': statuses[i]
-            }
+            d["id"]=ids[i]
+            d["num_votes"]=num_votes[i]
+            d["votes"]=votes[i]
+            d["lat"]=lats[i]
+            d["lng"]=lngs[i]
+            d["name"]=names[i]
+            d["bikes"]=self.tobike_bikes(bikes[i])
+            d["address"]=addresses[i]
+            d["status"]=statuses[i]
             l.insert(i,d)
             d={}
-    
+        
         return json.dumps(l)
         
+    def close_connection(self):
+        self.session.close()
+
 if __name__ == "__main__":
     TB = TObike("tobike","torino")
-    while(True) :
-        DBinsert(TB.city,TB.provider,TB.get_data())
-	time.sleep(60)
-    
+    f=True
+    while (f):
+        try:
+            TB.get_session(tobike_url)
+            current_state = TB.get_data()
+            DB_insert(TB.provider,TB.city,current_state)
+            write_log(TB.provider + " " + TB.city + ": state successfully loaded\n")
+            print "Data taken. "
+        except:
+            write_log(TB.provider + " " + TB.city + ": any operation has been done!\n")
 
-
-    
-    
+        #f=False
+        time.sleep(60)
+    print "Scrarper terminated."
+#    f.close()
+#    f= open("/home/mc/Scrivania/tobike/tobike.log","r")
+#    s = f.read()
+#    readRecord = json.loads(s)
+#    listOfBikes = json.loads(readRecord["state"])
